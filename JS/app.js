c5728e9421863c70cf059dac12242bce90cb01fb
Andres Pernas <ing.andrespernas@gmail.com>
2023-08-31 18:30:23 -0300

    Code review changes

--- JS/app.js
+++ JS/app.js
@@ -4,30 +4,15 @@ let guestScr = document.getElementById("guest-scr");
 let homeScore = 0;
 let guestScore = 0;
 
-function onePointH() {
-    homeScore += 1;
+function addPointsHome(number) {
+    homeScore += number;
     homeScr.textContent = homeScore;
 }
-function twoPointsH() {
-    homeScore += 2;
+function addPointsAway(number) {
+    homeScore += number;
     homeScr.textContent = homeScore;
 }
-function threePointsH() {
-    homeScore += 3;
-    homeScr.textContent = homeScore;
-}
-function onePointG() {
-    guestScore += 1;
-    guestScr.textContent = guestScore;
-}
-function twoPointsG() {
-    guestScore += 2;
-    guestScr.textContent = guestScore;
-}
-function threePointsG() {
-    guestScore += 3;
-    guestScr.textContent = guestScore;
-}
+
 
 function reset() {
     homeScore = 0;
@@ -36,3 +21,4 @@ function reset() {
     guestScr.textContent = guestScore;
 
 }
+
